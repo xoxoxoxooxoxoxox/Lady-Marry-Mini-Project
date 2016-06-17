da7c3bf9acbd4d412f369f743ea9f475d4480d44
xoxoxoxooxoxoxox <xoxoxoxooxoxoxox@gmail.com>
2016-06-16 17:55:38 -0700

    Complete Login UI
    Add LoginViewController on Main.storyboard
    Separate files into MVC framework

---
+++ littleBird/littleBird/LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  littleBird
+//
+//  Created by Chin-Chwen Tien on 6/16/16.
+//  Copyright © 2016 CHIN-CHWEN TIEN. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    // MARK: Properties
+    
+    @IBOutlet weak var debugTextLabel: UILabel!
+    @IBOutlet weak var loginButton: BorderedButton!
+    
+    override func viewDidLoad() {
+        print("LoginViewController viewDidLoad Called")
+        super.viewDidLoad()
+    }
+
+    
+
+
+}
+
